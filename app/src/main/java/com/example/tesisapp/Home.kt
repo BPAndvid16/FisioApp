798116e6ca928fb9019ac25e9bab645c1c1c6fef
BPAndvid16 <biomedicalpeople@gmail.com>
2021-08-03 14:39:26 -0500

    Actividades de video,chat y terapia, se continuara con conexion a cloudStorage Firebase

--- app/src/main/java/com/example/tesisapp/Home.kt
+++ app/src/main/java/com/example/tesisapp/Home.kt
@@ -1,11 +1,42 @@
 package com.example.tesisapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 
 class Home : AppCompatActivity() {
+
+    private lateinit var btnVideo: ImageButton
+    private lateinit var btnTerapia: ImageButton
+    private lateinit var btnChat: ImageButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        btnVideo = findViewById(R.id.btnvideo)
+        btnTerapia = findViewById(R.id.btnterapia)
+        btnChat = findViewById(R.id.btnchat)
+
+        btnVideo.setOnClickListener {
+            val videoIntent: Intent = Intent(this,Galeria::class.java)
+            startActivity(videoIntent)
+        }
+
+        btnTerapia.setOnClickListener {
+            val terapiaIntent: Intent = Intent(this,Terapias::class.java)
+            startActivity(terapiaIntent)
+        }
+
+        btnChat.setOnClickListener {
+            val chatIntent: Intent = Intent(this,Chat::class.java)
+            startActivity(chatIntent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/tesisapp/Terapias.kt
@@ -0,0 +1,11 @@
+package com.example.tesisapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Terapias : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terapias)
+    }
+}
