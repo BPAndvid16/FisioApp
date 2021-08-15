f7221389de1b5020d9fa9e8d7ca42a23f74900ec
Andres David Silva Naranjo <andresnaranjo@MacBook-Air-de-Andres.local>
2021-08-15 15:33:50 -0500

    proceso de acelerometro

--- app/src/main/java/com/example/tesisapp/Terapias.kt
+++ app/src/main/java/com/example/tesisapp/Terapias.kt
@@ -1,11 +1,69 @@
 package com.example.tesisapp
 
+import android.content.Context
+import android.icu.util.TimeUnit.values
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import java.time.chrono.JapaneseEra.values
+
+import android.app.Activity
+import android.hardware.*
+import android.hardware.SensorEventListener
+import android.widget.Button
+import kotlin.properties.Delegates
+import android.hardware.SensorEvent
+
+import android.hardware.SensorManager
+import android.widget.Toast
+
+
+interface SensorEventListener
+
+
 
 class Terapias : AppCompatActivity() {
+
+
+    private lateinit var Angulos: TextView
+    private lateinit var btn: Button
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terapias)
-    }
+
+
+
+        Angulos = findViewById(R.id.txtangulos)
+        btn = findViewById(R.id.btnsensar)
+
+
+
+
+
+
+btn.setOnClickListener{
+
+    val text = "Entro"
+    val duration = Toast.LENGTH_SHORT
+
+    val toast = Toast.makeText(applicationContext, text, duration).show()
+
+
+
 }
+
+
+
+    }
+
+
+
+}
+
+
