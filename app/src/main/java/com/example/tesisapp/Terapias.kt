0971657e9af006ab05ef07afb9423db2fdb22be7
Andres David Silva Naranjo <andresnaranjo@MacBook-Air-de-Andres.local>
2021-08-23 11:30:12 -0500

    Aplicacion sensando grados de inclinacion

--- app/src/main/java/com/example/tesisapp/Terapias.kt
+++ app/src/main/java/com/example/tesisapp/Terapias.kt
@@ -15,19 +15,46 @@ import kotlin.properties.Delegates
 import android.hardware.SensorEvent
 
 import android.hardware.SensorManager
+import android.view.View
 import android.widget.Toast
 
 
-interface SensorEventListener
 
 
+open class Terapias : AppCompatActivity(), SensorEventListener{
 
-class Terapias : AppCompatActivity() {
+    //Views
+
+    private var tvGyro: ArrayList<TextView> = ArrayList();
+
+    //Buttons
+
+    private lateinit var btnStart: Button
+    private lateinit var btnStop: Button
+    private lateinit var btnReset: Button
 
 
-    private lateinit var Angulos: TextView
-    private lateinit var btn: Button
+    //Viewid
 
+    private var idGyro: ArrayList<Int> = arrayListOf(R.id.tv_gy_x,R.id.tv_gy_y,R.id.tv_gy_z)
+
+    //Sensores
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sensorGyro: Sensor
+
+
+    //SensorData
+
+    private var gyroData: SensorData? = null
+
+    //GyroDaten
+
+    private var gyroX: Float = 0f
+    private var gyroY: Float = 0f
+    private var gyroZ: Float = 0f
+
+    private var timeGyro: Long = 0
 
 
 
@@ -39,31 +66,107 @@ class Terapias : AppCompatActivity() {
 
 
 
-        Angulos = findViewById(R.id.txtangulos)
-        btn = findViewById(R.id.btnsensar)
+        initViews()
+        initSensor()
 
+    }
 
+    private fun initViews(){
+        for (i in idGyro){
+            tvGyro.add(findViewById(i))
+        }
 
+        btnStart = findViewById(R.id.btnStart)
+        btnStop = findViewById(R.id.btnStop)
+        btnReset = findViewById(R.id.btnReset)
 
+        btnStart.setOnClickListener{
+            registerListener()
+            btnStart.isEnabled = false
+            btnStop.isEnabled = true
 
+        }
 
-btn.setOnClickListener{
+        btnStop.setOnClickListener {
 
-    val text = "Entro"
-    val duration = Toast.LENGTH_SHORT
+            unregisteredListener()
+            btnStart.isEnabled = true
+            btnStop.isEnabled = false
+        }
 
-    val toast = Toast.makeText(applicationContext, text, duration).show()
-
-
-
-}
-
+        btnReset.setOnClickListener {
+            gyroX = 0f
+            gyroY = 0f
+            gyroZ = 0f
+        }
 
 
     }
 
+    private fun initSensor(){
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null){
+
+            sensorGyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+
+
+
+        }
+    }
+
+    private fun registerListener(){
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null){
+
+            sensorManager.registerListener(this,sensorGyro,SensorManager.SENSOR_DELAY_NORMAL)
+
+        }
+
+    }
+
+    private fun unregisteredListener(){
+        sensorManager.unregisterListener(this,sensorGyro)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event!!.sensor.type == Sensor.TYPE_GYROSCOPE){
+            getGyroData(event)
+        }
+    }
+
+    override fun onAccuracyChanged(event: Sensor?, p1: Int) {
+
+    }
+
+
+    private fun getGyroData(e:SensorEvent?){
+
+        if(gyroData == null) {
+            gyroData = SensorData(e!!.values[0], e!!.values[1], e!!.values[2], e!!.timestamp)
+            timeGyro = System.currentTimeMillis()
+
+        }
+        else{
+            var time = (System.currentTimeMillis() - timeGyro)/1000f
+            gyroData!!.x1 = e!!.values[0]
+            gyroData!!.x2 = e!!.values[1]
+            gyroData!!.x3 = e!!.values[2]
+            gyroX += gyroData!!.x1 * time
+            gyroY = gyroData!!.x2 * time
+            gyroZ = gyroData!!.x3 * time
+        }
+
+        tvGyro[0].text = "x1: ${"%.2f".format(gyroData!!.x1*(180.0/Math.PI))} grados \t\t gyroX: ${"%.2f".format(gyroX*(180.0/Math.PI))} grados"
+        tvGyro[1].text = "x1: ${"%.2f".format(gyroData!!.x2*(180.0/Math.PI))} grados \t\t gyroY: ${"%.2f".format(gyroY*(180.0/Math.PI))} grados"
+        tvGyro[2].text = "x1: ${"%.2f".format(gyroData!!.x3*(180.0/Math.PI))} grados \t\t gyroZ: ${"%.2f".format(gyroZ*(180.0/Math.PI))} grados"
+        timeGyro = System.currentTimeMillis()
+    }
 
 }
+
+
+
+
 
 
